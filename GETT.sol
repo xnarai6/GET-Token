2694622eb397636d706fba5ed767edacb8b120ef
kim <39521944+xnarai6@users.noreply.github.com>
2022-06-23 23:03:59 +0900

    Add files via upload

--- GETT.sol
+++ GETT.sol
@@ -385,8 +385,6 @@ contract MintableToken is StandardToken, Ownable {
     balances[_to] = balances[_to].add(_amount);
     emit Mint(_to, _amount);
     emit Transfer(address(0), _to, _amount);
-    mintingFinished = true;
-    emit MintFinished();
     return true;
   }
 
