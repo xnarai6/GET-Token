92dd0a8272652aeec2186e67cb2b9047906a60b4
kim <39521944+xnarai6@users.noreply.github.com>
2022-06-23 16:44:18 +0900

    Add files via upload

--- GETT.sol
+++ GETT.sol
@@ -342,6 +342,67 @@ contract Ownable {
   }
 }
 
+// File: openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol
+
+/**
+ * @title Mintable token
+ * @dev Simple ERC20 Token example, with mintable token creation
+ * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
+ */
+contract MintableToken is StandardToken, Ownable {
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+
+  bool public mintingFinished = false;
+
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  modifier hasMintPermission() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(
+    address _to,
+    uint256 _amount
+  )
+    public
+    hasMintPermission
+    canMint
+    returns (bool)
+  {
+    totalSupply_ = totalSupply_.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    emit Mint(_to, _amount);
+    emit Transfer(address(0), _to, _amount);
+    mintingFinished = true;
+    emit MintFinished();
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+
+  function finishMinting() public onlyOwner canMint returns (bool) {
+    mintingFinished = true;
+    emit MintFinished();
+    return true;
+  }
+ 
+}
+
 // File: openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol
 
 /**
@@ -587,11 +648,16 @@ contract CanReclaimToken is Ownable {
 // empty block is used as this contract just inherits others.
 contract OwnableContract is CanReclaimToken, Claimable { } /* solhint-disable-line no-empty-blocks */
 
-contract GETT is StandardToken, DetailedERC20("Green Earch Coin", "GECTT", 18),
-     BurnableToken, PausableToken, OwnableContract {
+
+contract GETT is StandardToken, DetailedERC20("Green Earth community Token", "GETT", 18),
+    MintableToken, BurnableToken, PausableToken, OwnableContract {
 
     function burn(uint value) public onlyOwner {
         super.burn(value);
+    }
+
+    function finishMinting() public onlyOwner returns (bool) {
+        return false;
     }
 
     function renounceOwnership() public onlyOwner {
